0fedbdbba31a88982875a17706918f7e7f303b87
Nachoxt17 <ignacioceaglio@gmail.com>
2021-02-17 16:39:52 -0300

    2nd Commit

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-undef */
 const Decentragram = artifacts.require("Decentragram");
 
-module.exports = function(deployer) {
-  // Code goes here...
+module.exports = function (deployer) {
+  deployer.deploy(Decentragram);
 };
--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  // Code goes here...
+  string public name = "Decentragram";
 }
