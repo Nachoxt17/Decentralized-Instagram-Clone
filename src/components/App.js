7e1a64ae3f19fd06eda612e50ffbd78614059e3d
Nachoxt17 <ignacioceaglio@gmail.com>
2021-02-18 17:03:15 -0300

    6th Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,59 @@
-import React, { Component } from 'react';
-import Web3 from 'web3';
-import Identicon from 'identicon.js';
-import './App.css';
-import Decentragram from '../abis/Decentragram.json'
-import Navbar from './Navbar'
-import Main from './Main'
-
+import React, { Component } from "react";
+import Web3 from "web3";
+import Identicon from "identicon.js";
+import "./App.css";
+import Decentragram from "../abis/Decentragram.json";
+import Navbar from "./Navbar";
+import Main from "./Main";
 
 class App extends Component {
+  //+-How to Connect our Project with MetaMask:_ {
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    //+-Load account:_
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+  }
+  //+- }.
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      account: '',
-    }
+      account: "",
+    };
   }
 
   render() {
     return (
       <div>
         <Navbar account={this.state.account} />
-        { this.state.loading
-          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
-          : <Main
-            // Code...
-            />
-          }
-        }
+        {this.state.loading ? (
+          <div id="loader" className="text-center mt-5">
+            <p>Loading...</p>
+          </div>
+        ) : (
+          <Main
+          // Code...
+          />
+        )}
       </div>
     );
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,45 @@
-import React, { Component } from 'react';
-import Identicon from 'identicon.js';
-import photo from '../photo.png'
+import React, { Component } from "react";
+import Identicon from "identicon.js";
+import photo from "../photo.png";
 
 class Navbar extends Component {
-
   render() {
     return (
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
+          href="https://www.linkedin.com/in/ignacioceaglio/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src={photo} width="30" height="30" className="d-inline-block align-top" alt="" />
-          Decentragram
+          <img
+            src={photo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt=""
+          />
+          Ignacio Ceaglio - Decentragram
         </a>
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <small className="text-secondary">
-              <small id="account">{'0x0'}</small>
+              <small id="account">{"0x0"}</small>
             </small>
-            { this.props.account
-              ? <img
-                className='ml-2'
-                width='30'
-                height='30'
-                src={`data:image/png;base64,${new Identicon(this.props.account, 30).toString()}`}
+            {this.props.account ? (
+              <img
+                className="ml-2"
+                width="30"
+                height="30"
+                src={`data:image/png;base64,${new Identicon(
+                  this.props.account,
+                  30
+                ).toString()}`}
+                alt="Decentraland Icon"
               />
-              : <span></span>
-            }
+            ) : (
+              <span></span>
+            )}
           </li>
         </ul>
       </nav>
