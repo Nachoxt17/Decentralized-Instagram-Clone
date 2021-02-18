75f36974fd68283877b3527d6f8b4743e688a9e4
Nachoxt17 <ignacioceaglio@gmail.com>
2021-02-18 00:04:56 -0300

    5th Commit

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -34,36 +34,45 @@ contract Decentragram {
   }
 
   function uploadImage(string memory _imgHash, string memory _description) public {
-    // Make sure the image hash exists
+    //+-Make sure the image hash exists:_
     require(bytes(_imgHash).length > 0);
-    // Make sure image description exists
+    
+    //+-Make sure image description exists:_
     require(bytes(_description).length > 0);
-    // Make sure uploader address exists
+
+    //+-Make sure uploader address exists:_
     require(msg.sender!=address(0));
 
-    // Increment image id
+    //+-Increment image id:_
     imageCount ++;
 
-    // Add Image to the contract
+    //+-Add Image to the contract:_
     images[imageCount] = Image(imageCount, _imgHash, _description, 0, msg.sender);
-    // Trigger an event
+
+    //+-Trigger an event:_
     emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
   }
 
   function tipImageOwner(uint _id) public payable {
-    // Make sure the id is valid
+    //+-Make sure the id is valid:_
     require(_id > 0 && _id <= imageCount);
-    // Fetch the image
+
+    //+-Fetch the image:_
     Image memory _image = images[_id];
-    // Fetch the author
+
+    //+-Fetch the author:_
     address payable _author = _image.author;
-    // Pay the author by sending them Ether
+
+    //+-Pay the author by sending them Ether:_
     address(_author).transfer(msg.value);
-    // Increment the tip amount
+
+    //+-Increment the tip amount:_
     _image.tipAmount = _image.tipAmount + msg.value;
-    // Update the image
+
+    //+-Update the image:_
     images[_id] = _image;
-    // Trigger an event
+
+    //+-Trigger an event:_
     emit ImageTipped(_id, _image.hash, _image.description, _image.tipAmount, _author);
   }
 }
